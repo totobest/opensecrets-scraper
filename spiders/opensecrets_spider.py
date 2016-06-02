0398a10cd1c7b33b25e13e5f4edd521fb36b8eb7
Tony <tony.lbvre@gmail.com>
2016-06-01 23:48:14 -0700

    Truncate to 50 words

--- spiders/opensecrets_spider.py
+++ spiders/opensecrets_spider.py
@@ -1,8 +1,11 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
+
 import datetime
+import itertools
 from itertools import izip_longest
 
+import re
 import scrapy
 from slugify import slugify
 
@@ -14,11 +17,14 @@ def grouper(iterable, n, fillvalue=None):
     return izip_longest(fillvalue=fillvalue, *args)
 
 
-def smart_truncate(content, length=100, suffix='...'):
-    if len(content) <= length:
-        return content
-    else:
-        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+def split_iter(string):
+    return (x.group(0) for x in re.finditer(r"[^ ]+", string))
+
+
+def smart_truncate(content, length=50, suffix='...'):
+    words_list = list(itertools.islice(split_iter(content), length + 1))
+    res = ' '.join(words_list[:length])
+    return res + suffix if len(words_list) > length else res
 
 
 class OpenSecretsSpider(scrapy.Spider):
@@ -42,4 +48,4 @@ class OpenSecretsSpider(scrapy.Spider):
 
                     }
                 }
-        }
+            }
